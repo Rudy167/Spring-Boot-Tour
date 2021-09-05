18ec6cadced80be5be1b038dd1ceb6f1fec4aff2
Rudy167 <amitdhotay121@gmail.com>
2021-09-05 20:36:19 +0530

    first commit

---
+++ src/main/java/com/example/ec/repo/TourPackageRepository.java
@@ -0,0 +1,20 @@
+package com.example.ec.repo;
+
+import com.example.ec.domain.TourPackage;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
+
+
+public interface TourPackageRepository extends CrudRepository<TourPackage, String> {
+
+    /**
+     * Find Tour Package by name.
+     *
+     * @param name name of the package
+     * @return Optional of TourPackage
+     */
+    Optional<TourPackage> findByName(@Param("name")String name);
+}
+
